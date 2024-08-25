54d2b9a00987be21bae362412bfd6913d6239341
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-08-24 21:38:42 -0500

    commiting changes before i mess something up like i always do in life

--- src/server/build.js
+++ src/server/build.js
@@ -1,5 +1,8 @@
 const fs = require("fs")
 const path = require("path")
+const util = require("util")
+
+let readFileAsync = util.promisify(fs.readFile)
 async function startBuild(){
     console.time("BuildCopy")
     const publicDir = path.resolve("./public")
@@ -10,6 +13,43 @@ async function startBuild(){
     console.timeEnd("BuildCopy")
 
     console.time("BuildInject")
+    let buildPromises = fs.readdirSync(buildDir, {recursive: true}).map((relativeFilePath)=>{
+        return new Promise(async(r)=>{
+            try{
+                let filePath = path.join(buildDir, relativeFilePath)
+                let extName = path.extname(relativeFilePath)
+                let fileData = await readFileAsync(filePath, "utf-8")
+                switch(extName){
+                    case(".html"):
+                            fileData = fileData.replace("<!-- INJECT_NAV -->", `
+                            <div id="nav">
+                            <img class="logo" src="img/logo.svg" onclick="window.location.assign('./')">
+                            ${filePath=="game.html"?'<input placeholder="Search" type="text" class="searchBar" id="search">':""}
+                            <a href="game.html" style="margin:2vmin;">Games</a>
+                            <a href="login.html" style="margin:2vmin;">Admin Login</a>
+                            </nav>
+                        `)
+                        if(relativeFilePath=="game.html"){
+
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                fs.writeFileSync(filePath, fileData)
+                console.log(relativeFilePath+" completed")
+                r("Success")
+            }catch(err){
+                // console.warn(err)
+
+                console.log(relativeFilePath+" failed")
+                r("Fail")
+            }
+
+        })
+    })
+    await Promise.all(buildPromises)
 
     console.timeEnd("BuildInject")
 }
